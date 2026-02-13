10abb0757326e23d0faa469650cd583fb019ea51
Natdanai Chuwongapichart <natdanaichuwongapichat@Natdanais-MacBook-Pro.local>
2026-02-13 22:51:23 +0700

    finished fr

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { merge } from './merge';
-//# sourceMappingURL=index.d.ts.map
--- dist/merge.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function merge(collection_1: number[], collection_2: number[], collection_3: number[]): number[];
-//# sourceMappingURL=merge.d.ts.map
